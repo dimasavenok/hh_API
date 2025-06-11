ff4121745ffa55efe48c8e80bc77601ca86abb0e
dimasavenok <dimas.savenok1990@gmail.com>
2025-06-11 19:28:05 +0300

    fix class API

--- src/api/base_api.py
+++ src/api/base_api.py
@@ -1,8 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict
 
+import requests
+
+
 
 class BaseAPI(ABC):
+    @abstractmethod
+    def __connect(self, keyword: str) -> requests.Response:
+        pass
+
+
     @abstractmethod
     def get_vacancies(self, keyword: str) -> List[Dict[Any, Any]]:
         pass
--- src/api/hh_api.py
+++ src/api/hh_api.py
@@ -2,16 +2,29 @@ from typing import Any, Dict, List
 
 import requests
 
+
 from src.api.base_api import BaseAPI
 
 
 class HeadHunterAPI(BaseAPI):
-    def get_vacancies(self, keyword: str) -> List[Dict[Any, Any]]:
-        url: str = "https://api.hh.ru/vacancies"
+    __slots__ = ("__base_url", "__headers")
+
+
+    def __init__(self, file_worker):
+        self.__base_url = 'https://api.hh.ru'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+
+
+    def __connect(self, keyword: str) -> requests.Response:
         params: Dict[str, str | int] = {
             "text": keyword,
             "area": 1,
             "per_page": 100,
         }
-        response: requests.Response = requests.get(url, params=params)
-        return response.json().get("items", [])
+        response: requests.Response = requests.get(f"{self.__base_url}/vacancies", headers=self.__headers, params=params)
+        response.raise_for_status()
+        return response
+
+
+    def get_vacancies(self, keyword: str) -> List[Dict[Any, Any]]:
+        return self.__connect(keyword).json().get("items", [])
