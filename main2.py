434f413f37c1e587ce3094d4fb5b617c68da1737
dimasavenok <dimas.savenok1990@gmail.com>
2025-07-10 21:01:49 +0300

    next lesson

--- main2.py
+++ main2.py
@@ -2,21 +2,36 @@ import json
 
 from src.storage.DBManager import DBManager
 
-# connect_params = {
-#     "dbname": "hh_db",
-#     "user": "postgres",
-#     "password": "hh_password",
-#     "host": "localhost",
-#     "port": "5432"
-# }
-with open("params.json", mode="r", encoding="UTF-8") as file:
-    connect_params = json.load(file)
+connect_params = {
+    "dbname": "hh_db",
+    "user": "postgres",
+    "password": "dima510141",
+    "host": "localhost",
+    "port": "5432"
+}
+
 db: DBManager = DBManager(connect_params)
-# db.insert_company("K-test")
-# db.insert_vacancy(
-#     title="py",
-#     salary=100,
-#     url="ddddd",
-#     description="fffff",
-#     company_name="K-test"
-# )
+db.insert_company("K-test")
+db.insert_vacancy(
+    title="py",
+    salary=100,
+    url="ddddd",
+    description="fffff",
+    company_name="K-test"
+)
+
+db.insert_vacancy(
+    title="py2",
+    salary=200,
+    url="ddddd",
+    description="fffff",
+    company_name="K-test"
+)
+for name, count in db.get_companies_and_vacancies_count():
+    print(f"{name}: {count} вакансий")
+print("----------")
+print(db.get_all_vacancies())
+print("----------")
+print(db.get_avg_salary())
+print("----------")
+print(db.get_vacancies_with_higher_salary())
--- src/storage/DBManager.py
+++ src/storage/DBManager.py
@@ -1,5 +1,4 @@
-﻿from typing import Self, Optional
-
+﻿from typing import Self, Optional, Any
 import psycopg2
 from psycopg2.extensions import connection
 
@@ -8,9 +7,8 @@ class DBManager:
     def __init__(self: Self, connect_params: dict[str, str]) -> None:
         self.connect_params: dict[str, str] = connect_params
         self.conn: connection = self.connect()
-        # self.__reset_database()
-        # self.__create_tables()
-
+        self.__reset_database()
+        self.__create_tables()
 
     def connect(self) -> connection:
         try:
@@ -21,18 +19,10 @@ class DBManager:
                 host=self.connect_params.get("host"),
                 port=self.connect_params.get("port")
             )
-            # return psycopg2.connect(
-            #     dbname="hh_db",
-            #     user="postgres",
-            #     password="hh_password",
-            #     host="localhost",
-            #     port="5432"
-            # )
         except psycopg2.Error:
             raise
         except Exception:
             raise
-
 
     def __reset_database(self):
         with self.conn.cursor() as cur:
@@ -40,48 +30,46 @@ class DBManager:
             cur.execute("DROP TABLE IF EXISTS companies CASCADE;")
         self.conn.commit()
 
-
     def __create_tables(self):
         with self.conn.cursor() as cur:
             cur.execute(
                 """
-                CREATE TABLE IF NOT EXISTS companies 
-                (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL UNIQUE
+                CREATE TABLE IF NOT EXISTS companies (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL UNIQUE
                 );
                 """
             )
-
             cur.execute(
                 """
-                CREATE TABLE IF NOT EXISTS vacancies
-                (
-                id SERIAL PRIMARY KEY,
-                company_id INTEGER REFERENCES companies(id) ON DELETE CASCADE,
-                title VARCHAR(255) NOT NULL,
-                salary FLOAT DEFAULT NULL,
-                url TEXT NOT NULL 
-                description TEXT DEFAULT NULL
+                CREATE TABLE IF NOT EXISTS vacancies (
+                    id SERIAL PRIMARY KEY,
+                    company_id INTEGER REFERENCES companies(id) ON DELETE CASCADE,
+                    title VARCHAR(255) NOT NULL,
+                    salary FLOAT DEFAULT NULL,
+                    url TEXT NOT NULL,
+                    description TEXT DEFAULT NULL
                 );
-                """)
+                """
+            )
         self.conn.commit()
 
     def insert_company(self, name: str):
         with self.conn.cursor() as cur:
             cur.execute("""
-            INSERT INTO companies (name)
-            VALUES (%s);
+                INSERT INTO companies (name)
+                VALUES (%s)
+                ON CONFLICT (name) DO NOTHING;
             """, (name,))
         self.conn.commit()
 
     def insert_vacancy(
-            self,
-            title: str,
-            salary: float,
-            url: str,
-            description: str,
-            company_name: str
+        self,
+        title: str,
+        salary: float,
+        url: str,
+        description: str,
+        company_name: str
     ):
         company_id = self.get_company_id_by_name(company_name)
         if company_id is None:
@@ -91,16 +79,58 @@ class DBManager:
             return
         with self.conn.cursor() as cur:
             cur.execute("""
-            INSERT INTO vacancies (company_id, title, salary, url, description)
-            VALUES (%s, %s, %s, %s, %s);
+                INSERT INTO vacancies (company_id, title, salary, url, description)
+                VALUES (%s, %s, %s, %s, %s);
             """, (company_id, title, salary, url, description))
         self.conn.commit()
 
-
-    def get_company_id_by_name(self, name) -> Optional[int]:
+    def get_company_id_by_name(self, name: str) -> Optional[int]:
         with self.conn.cursor() as cur:
             cur.execute("""
-                        SELECT id FROM companies
-                        WHERE name = "%s";
-                        """, (name,))
-        self.conn.commit()
+                SELECT id FROM companies
+                WHERE name = %s;
+            """, (name,))
+            result = cur.fetchone()
+            return result[0] if result else None
+
+    def get_companies_and_vacancies_count(self) -> Optional[list[tuple[str, int]]]:
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                SELECT c.name, COUNT(v.id) AS vacancy_count 
+                FROM companies c
+                LEFT JOIN vacancies v ON c.id = v.company_id
+                GROUP BY c.id
+                ORDER BY vacancy_count DESC;
+            """ )
+            return cur.fetchall()
+
+    def get_all_vacancies(self) -> Optional[list[tuple[Any]]]:
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                       SELECT c.name, v.title, v.salary, v.url
+                       FROM vacancies v
+                       LEFT JOIN companies c ON c.id = v.company_id;
+                   """)
+            return cur.fetchall()
+
+    def get_avg_salary(self) -> Optional[float]:
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                SELECT AVG(salary)
+                FROM vacancies
+                WHERE salary IS NOT NULL;
+            """)
+            result = cur.fetchone()
+            return result[0] if result else None
+
+    def get_vacancies_with_higher_salary(self) -> list[tuple[str, str, float]]:
+        avg_salary = self.get_avg_salary()
+        if avg_salary is None: return []
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                SELECT c.name, v.title, v.salary
+                FROM vacancies v
+                LEFT JOIN companies c ON c.id = v.company_id
+                WHERE v.salary > %s;
+            """, (avg_salary,))
+            return cur.fetchall()
